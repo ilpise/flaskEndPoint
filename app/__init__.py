49dfb9f5c6cae7c395003bcfa84d0c05415709f7
ilpise <pisewip@yahoo.it>
2021-07-13 17:03:03 +0200

    Added thread (daemon) to the flask app with async request to modbus

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-
+import logging
+import threading
+import time
+import random
+import asyncio
+from pymodbus.client.asynchronous.tcp import AsyncModbusTCPClient as ModbusClient
+from pymodbus.client.asynchronous import schedulers
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -13,13 +19,65 @@ login_manager = LoginManager()
 csrf_protect = CSRFProtect()
 migrate = Migrate()
 
+UNIT = 0x1
+
+
+# Probabilmente questo è un worker
+# vedi https://tutorialedge.net/python/concurrency/asyncio-event-loops-tutorial/
+async def start_async_test(client):
+    rr = await client.read_holding_registers( 1, 8, unit=UNIT )
+    print( rr.registers )
+    await asyncio.sleep(1)
+
+# data_store = {'a': 1}
+# def interval_query():
+#     while True:
+#         time.sleep( 5 )
+#         vals = {'a': random.randint( 0, 100 )}
+#         print( vals )
+#         data_store.update( vals )
+
+def test_modbus_thread():
+    while True:
+        print("---------------------RUN_WITH_NO_LOOP-----------------")
+        loop, client = ModbusClient(schedulers.ASYNC_IO, port=5021)
+        loop.run_until_complete(start_async_test(client.protocol))
+        loop.close()
+        print("--------DONE RUN_WITH_NO_LOOP-------------")
+
 def create_app(config_class=DevelopmentConfig):
+
+    # Asinc modbus stuff
+    # run_with_no_loop()
+    # Run with loop not yet started
+    # run_with_not_running_loop()
+
+    # Test using threading
+    # Questo funziona - se lanciamo l'applicazione vengono printati i vals nel log
+    # e l'applicazione web funziona contemporaneamente
+    # data_store = {'a': 1}
+    # def interval_query():
+    #     while True:
+    #         time.sleep( 5 )
+    #         vals = {'a': random.randint( 0, 100 )}
+    #         print(vals)
+    #         data_store.update( vals )
+
+    # thread = threading.Thread( name='interval_query', target=interval_query )
+    thread = threading.Thread( name='test_modbus_thread', target=test_modbus_thread )
+    thread.setDaemon( True )
+    thread.start()
+
+
+    # logging.info( 'Started' )
     # Instantiate Flask
     app = Flask(__name__,
                 static_folder='./freelancer',
                 # static_folder='./oldStatic',
                 # template_folder='./app/templates'
                 )
+    logging.basicConfig( filename='myapp.log', level=logging.INFO )
+    # logger = logging.getLogger( __name__ )
 
     app.config.from_object(config_class)
     # Database to use flask db
