e65657be635288aff6bb4e54dbcc7d50435fbbf6
ilpise <pisewip@yahoo.it>
2021-08-18 15:35:53 +0200

    Added test route for smart card module testing on raspberry

--- app/controllers/controller1.py
+++ app/controllers/controller1.py
@@ -14,6 +14,10 @@ import uuid, json, os
 import datetime
 import requests
 
+# Smartcard
+from smartcard.System import readers
+import array
+
 
 # When using a Flask app factory we must use a blueprint to avoid needing 'app' for '@app.route'
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
@@ -58,7 +62,80 @@ def member_page():
     return render_template('views/controller1/member_base.html')
 
 
-# @main_blueprint.route("/settings")
-# @login_required
-# def settings():
-#     pass
+@main_blueprint.route("/testsc")
+def testsc():
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if not current_user.is_authenticated:
+        return redirect(url_for('main.login'))
+
+    r = readers()
+    print( r )
+    reader = r[0]
+    print( "Sto usando: ", reader )
+    connection = reader.createConnection()
+    connection.connect()
+    # Seleziona del MF
+    # CLS 00, istruzione A4 (seleziona file), P1 = P2 = 0 (seleziona per ID),
+    # Lc: 2, Data: 3F00 (id del MF)
+    SELECT_MF = [0x00, 0xA4, 0x00, 0x00, 0x02, 0x3F, 0x00]
+    data, sw1, sw2 = connection.transmit( SELECT_MF )
+    # se tutto è andato a buon fine sw1 e sw2 contengono
+    # rispettivamente i valori 0x90 e 0x00 il corrispettivo del 200 in HTTP
+
+    # Seleziona del DF1...vedi sopra
+    SELECT_DF1 = [0x00, 0xA4, 0x00, 0x00, 0x02, 0x11, 0x00]
+    data, sw1, sw2 = connection.transmit( SELECT_DF1 )
+
+    # Seleziona del file EF.Dati_personali... vedi sopra sopra
+    SELECT_EF_PERS = [0x00, 0xA4, 0x00, 0x00, 0x02, 0x11, 0x02]
+    data, sw1, sw2 = connection.transmit( SELECT_EF_PERS )
+
+    # leggiamo i dati
+    # CLS 00, istruzione B0 (leggi i dati binari contenuti nel file
+    READ_BIN = [0x00, 0xB0, 0x00, 0x00, 0x00, 0x00]
+    data, sw1, sw2 = connection.transmit( READ_BIN )
+    # data contiene i dati anagrafici in formato binario
+    # trasformiamo il tutto in una stringa
+    # stringa_dati_personali = array.array('B', data).tostring()
+    stringa_dati_personali = array.array( 'B', data ).tobytes()
+
+    prox_field_size = int( stringa_dati_personali[6:8], 16 )
+    da = 8
+    a = da + prox_field_size
+    if prox_field_size > 0:
+        codice_emettitore = stringa_dati_personali[da:a]
+        print( "Codice emettitore: ", codice_emettitore.decode( "utf-8" ) )
+
+    da = a
+    a += 2
+    prox_field_size = int( stringa_dati_personali[da:a], 16 )
+    da = a
+    a += prox_field_size
+    if prox_field_size > 0:
+        data_rilascio_tessera = stringa_dati_personali[da:a]
+        print( "Data rilascio tessera: ",
+               data_rilascio_tessera[0:2].decode( "utf-8" ) + "/" + data_rilascio_tessera[2:4].decode(
+                   "utf-8" ) + "/" + data_rilascio_tessera[-4:].decode( "utf-8" ) )
+
+    da = a
+    a += 2
+    prox_field_size = int( stringa_dati_personali[da:a].decode( "utf-8" ), 16 )
+    da = a
+    a += prox_field_size
+    if prox_field_size > 0:
+        data_scadenza_tessera = stringa_dati_personali[da:a]
+        print( "Data scadenza tessera: ",
+               data_scadenza_tessera[0:2].decode( "utf-8" ) + "/" + data_scadenza_tessera[2:4].decode(
+                   "utf-8" ) + "/" + data_scadenza_tessera[-4:].decode( "utf-8" ) )
+
+    da = a
+    a += 2
+    prox_field_size = int( stringa_dati_personali[da:a].decode( "utf-8" ), 16 )
+    da = a
+    a += prox_field_size
+    if prox_field_size > 0:
+        cognome = stringa_dati_personali[da:a].decode( "utf-8" )
+        print( "Cognome: ", cognome )
+
+    return render_template( 'views/controller1/member_base.html' )
