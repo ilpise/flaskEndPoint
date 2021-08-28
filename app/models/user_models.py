859cbd2ed924f7709253e4ea92e6620f9f93dcf9
ilpise <pisewip@yahoo.it>
2021-08-28 12:50:21 +0200

    UserMixin from flask_user - AttributeError: type object 'User' has no attribute 'get_user_by_token'

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,10 +1,13 @@
 # Copyright 2021 Simone Corti . All rights reserved
 
-from flask_login import UserMixin, login_manager
+# from flask_login import UserMixin, login_manager
+from flask_user import UserMixin
 # from flask_user.forms import RegisterForm
 from flask_wtf import FlaskForm
+# from werkzeug.security import check_password_hash
 from wtforms import StringField, SubmitField, validators
 from app import db, login_manager
+from dataclasses import dataclass
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -12,7 +15,17 @@ def load_user(user_id):
     return User.query.filter_by(id=user_id).first()
 
 # Define the User data model. Make sure to add the flask_user.UserMixin !!
+@dataclass
 class User(db.Model, UserMixin):
+    id: int
+    active: bool
+    username: str
+    password: str
+    email: str
+    first_name: str
+    last_name: str
+    roles: str
+
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     # The active flag is 1 for DEFAULT this way the user is created active
@@ -34,7 +47,7 @@ class User(db.Model, UserMixin):
                             backref=db.backref('users', lazy='dynamic'))
     def has_role(self, role):
         for item in self.roles:
-            if item.name == 'admin':
+            if item.name == role:
                 return True
         return False
 
@@ -47,6 +60,13 @@ class User(db.Model, UserMixin):
         # return str(self.full_name)
         return str( self.username )
 
+    # def get_user_by_token(self):
+    #     return True
+
+    # https://python.plainenglish.io/implementing-flask-login-with-hash-password-888731c88a99
+    # Default check_password_hash from werkzeug.security
+    # def verify_password(self, pwd):
+    #     return check_password_hash( self.password, pwd )
 
 # Define the Role data model
 class Role(db.Model):
