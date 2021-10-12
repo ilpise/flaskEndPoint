4338e452da91ae0bf253aeeb06168ae518a60b49
ilpise <pisewip@yahoo.it>
2021-10-12 14:55:18 +0200

    Added configuration elements for COGES && MQTT

--- config.py
+++ config.py
@@ -28,6 +28,20 @@ class DevelopmentConfig(ProductionConfig):
     OPENPLC_IP = '172.20.0.3'
     OPENPLC_MODBUS_PORT = 502
 
+    # COGES
+    COGES_PORT = '/dev/ttyACM0'
+    COGES_BAUDRATE = 9600
+    # NEBULAR_MERCHANT_ID = 'TODO'
+    # NEBULAR_AUTH_TOKEN = 'TODO'
+
+    # MQTT
+    # MQTT_CLIENT_ID = 'flaskEP-vendor-test'
+    MQTT_BROKER_URL = '85.94.200.117'
+    MQTT_BROKER_PORT = 1883  # default port for non-tls connection
+    # MQTT_USERNAME = ''  # set the username here if you need authentication for the broker
+    # MQTT_PASSWORD = ''  # set the password here if the broker demands authentication
+    MQTT_KEEPALIVE = 5  # set the time interval for sending a ping to the broker to 5 seconds
+    MQTT_TLS_ENABLED = False  # set TLS to disabled for testing purposes
 
 # class TestingConfig(ProductionConfig):
 #     DEBUG = False
